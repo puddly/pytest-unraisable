ee74884ff707909fefbd0f314811ceda2f1bada6
puddly <32534428+puddly@users.noreply.github.com>
2020-09-10 12:23:05 -0400

    The `test` package is meant for internal use by Python only.

--- pytest_unraisable.py
+++ pytest_unraisable.py
@@ -2,7 +2,6 @@ import gc
 import sys
 import warnings
 import contextlib
-import test.support
 
 import pytest
 
@@ -15,10 +14,31 @@ __short_version__ = f"{MAJOR_VERSION}.{MINOR_VERSION}"
 __version__ = f"{__short_version__}.{PATCH_VERSION}"
 
 
+# Copied from the Python-internal `test.support.catch_unraisable_exception`
+class catch_unraisable_exception:
+    def __init__(self):
+        self.unraisable = None
+        self._old_hook = None
+
+    def _hook(self, unraisable):
+        # Storing unraisable.object can resurrect an object which is being
+        # finalized. Storing unraisable.exc_value creates a reference cycle.
+        self.unraisable = unraisable
+
+    def __enter__(self):
+        self._old_hook = sys.unraisablehook
+        sys.unraisablehook = self._hook
+        return self
+
+    def __exit__(self, *exc_info):
+        sys.unraisablehook = self._old_hook
+        del self.unraisable
+
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_pyfunc_call(pyfuncitem):
     if hasattr(sys, "unraisablehook"):
-        catch_unraisable = test.support.catch_unraisable_exception()
+        catch_unraisable = catch_unraisable_exception()
     else:
         catch_unraisable = contextlib.nullcontext()
 
